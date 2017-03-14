5d90355dfaf2c634640702747057163bb98e2654
Gustavo Coronel <gcoronelc@gmail.com>
2017-03-14 11:38:06 -0500

    Sem 01

---
+++ Sem01/PedidoApp/src/pe/egcc/pedidoapp/prueba/Prueba01.java
@@ -0,0 +1,20 @@
+package pe.egcc.pedidoapp.prueba;
+
+import pe.egcc.pedidoapp.service.PedidoService;
+
+public class Prueba01 {
+  
+  public static void main(String[] args) {
+    // Datos
+    double importe = 1000.0;
+    // Proceso
+    PedidoService service = new PedidoService();
+    double impuesto = service.calcularImpuesto(importe);
+    double total = service.calcularTotal(importe);
+    // Reporte
+    System.out.println("Importe: " + importe);
+    System.out.println("Impuesto: " + impuesto);
+    System.out.println("Total: " + total);
+  }
+  
+}

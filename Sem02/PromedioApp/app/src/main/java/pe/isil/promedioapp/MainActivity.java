fff9190b98452bb916c65d6f925b0f6c75388b52
Gustavo Coronel <gcoronelc@gmail.com>
2017-03-28 10:35:53 -0500

    Sem 02

--- Sem02/PromedioApp/app/src/main/java/pe/isil/promedioapp/MainActivity.java
+++ Sem02/PromedioApp/app/src/main/java/pe/isil/promedioapp/MainActivity.java
@@ -2,13 +2,50 @@ package pe.isil.promedioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import pe.isil.promedioapp.service.PedidoService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etImporte;
+    private TextView tvImporte;
+    private TextView tvImpuesto;
+    private TextView tvTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Controles
+        etImporte = (EditText) findViewById(R.id.etImporte);
+        tvImporte = (TextView) findViewById(R.id.tvImporte);
+        tvImpuesto = (TextView) findViewById(R.id.tvImpuesto);
+        tvTotal = (TextView) findViewById(R.id.tvTotal);
+
+        tvImporte.setText("Importe:");
+        tvImpuesto.setText("Impuesto:");
+        tvTotal.setText("Total:");
     }
+
+
+    public void doProcesar(View v){
+        // Input (Dato)
+        double importe = Double.parseDouble(etImporte.getText().toString());
+        // Proceso
+        PedidoService service = new PedidoService();
+        double impuesto = service.calcularImpuesto(importe);
+        double total = service.calcularTotal(importe);
+        // Output (Reporte)
+        tvImporte.setText("Importe: " + importe);
+        tvImpuesto.setText("Impuesto: " + impuesto);
+        tvTotal.setText("Total: " + total);
+
+    }
+
+
 
 }
---
+++ Sem02/PromedioApp/app/src/main/java/pe/isil/promedioapp/service/PedidoService.java
@@ -0,0 +1,18 @@
+package pe.isil.promedioapp.service;
+
+public class PedidoService {
+
+
+    public double calcularImpuesto(double importe){
+        double impuesto;
+        impuesto = importe * 0.18;
+        return impuesto;
+    }
+
+    public double calcularTotal(double importe){
+        double total;
+        total = importe + calcularImpuesto(importe);
+        return total;
+    }
+
+}
